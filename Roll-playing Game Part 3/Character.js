b13aeeb460884ab04a8de605fdc929e98c7bd01c
I-Slot <ismailadesholaharun@gmail.com>
2023-03-29 14:48:25 +0100

    Update Character.js

--- Roll-playing Game Part 3/Character.js
+++ Roll-playing Game Part 3/Character.js
@@ -1,8 +1,6 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
 
-
-
 const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth;
 
 
@@ -27,17 +25,29 @@ function Character(data) {
       this.dead = true;
       this.health = 0
     }
-    // console.log(getPercentage((this.maxHealth, this.health)));
+
+  }
+
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth)
+    return `
+      <div class="health-bar-outer">
+          <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+              style="width: ${percent}%;">
+          </div>
+      </div>`
   }
 
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
 
+    const healthBar = this.getHealthBarHtml()
     return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${this.diceArray}
                 </div>
