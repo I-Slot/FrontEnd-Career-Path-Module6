743bf08ca9848b00b768e31dfb5ea187c1fffc7a
I-Slot <ismailadesholaharun@gmail.com>
2023-03-31 18:45:43 +0100

    Update

---
+++ Part 4/classes/index.js
@@ -0,0 +1,10 @@
+class Module {
+    constructor() {
+        this.courseName = "Learn JS"
+        this.studentsEnrolled = 5600
+        this.studentsCompleted = 5100
+    }
+}
+
+const learnJs = new Module
+console.log(learnJs.courseName);
--- Roll-playing Game Part 3/Character.js
+++ Roll-playing Game Part 3/Character.js
@@ -1,51 +1,68 @@
-import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
+import {
+  getDiceRollArray,
+  getDicePlaceholderHtml,
+  getPercentage,
+} from "./utils.js";
 
-function Character(data) {
-    Object.assign(this, data)
-    this.maxHealth = this.health
+/*
+Challenge
+1. Convert the constructor function to a class.
 
-    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+    Think about:
+    1. Where do properties go?
+    2. Where do Methods go?
+*/
 
-    this.getDiceHtml = function () {
-        this.currentDiceScore = getDiceRollArray(this.diceCount)
-        this.diceArray = this.currentDiceScore.map((num) =>
-            `<div class="dice">${num}</div>`).join("")
+class Character {
+  constructor(data) {
+    Object.assign(this, data);
+    this.maxHealth = this.health;
+    this.diceHtml = getDicePlaceholderHtml(this.diceCount);
+  }
+
+  setDiceHtml() {
+    this.currentDiceScore = getDiceRollArray(this.diceCount);
+    this.diceHtml = this.currentDiceScore
+      .map((num) => `<div class="dice">${num}</div>`)
+      .join("");
+  }
+
+  takeDamage(attackScoreArray) {
+    const totalAttackScore = attackScoreArray.reduce(
+      (total, num) => total + num
+    );
+    this.health -= totalAttackScore;
+    if (this.health <= 0) {
+      this.dead = true;
+      this.health = 0;
     }
+  }
 
-    this.takeDamage = function (attackScoreArray) {
-        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
-        this.health -= totalAttackScore
-        if (this.health <= 0) {
-            this.dead = true
-            this.health = 0
-        }
-    }
-
-
-    this.getHealthBarHtml = function () {
-        const percent = getPercentage(this.health, this.maxHealth)
-        return `<div class="health-bar-outer">
-                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+  getHealthBarHtml() {
+    const percent = getPercentage(this.health, this.maxHealth);
+    return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${
+                      percent < 26 ? "danger" : ""
+                    }" 
                             style="width:${percent}%;">
                     </div>
-                </div>`  
-    }
-    
+                </div>`;
+  }
 
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this
-        const healthBar = this.getHealthBarHtml()
-        return `
+  getCharacterHtml() {
+    const { elementId, name, avatar, health, diceCount, diceHtml } = this;
+    const healthBar = this.getHealthBarHtml();
+    return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 ${healthBar}
                 <div class="dice-container">
-                    ${this.diceArray}
+                    ${diceHtml}
                 </div>
-            </div>`
-    }
+            </div>`;
+  }
 }
 
-export default Character
+export default Character;
